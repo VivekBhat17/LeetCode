9a62fb68b0b51e1d79c14a0cb82e671a4c46247e
Vivek <vivekbhat569@gmail.com>
2021-06-29 22:55:25 +0530

    master

---
+++ Reverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int ReverseDigit(int num){
+    int rem,rev=0;
+while (num > 0){
+    rem  = num %10 ;
+    num  = num/10;
+    rev  = rev*10 + rem;
+    }
+    return rev;
+}
+int main(){
+    int x = 234;
+    cout<<ReverseDigit(x)<<"\n";
+}
